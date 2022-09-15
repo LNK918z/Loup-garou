1f47ad1a6d11539de1ff830ee9ad7dc14faf210d
aeroget <65290168+aeroget@users.noreply.github.com>
2022-09-15 19:47:59 +0200

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+//For the start game
+function StartGame() {
+    let name = document.createElement("textarea".nickname);
+    name.style.position = "absolute";
+    name.style.left = "30px";
+    name.style.top = "0";
+    name.style.placeholder = "chose a nickname";
+    document.body.appendChild(name);
+    name.textContent = t;
+    name.select();
+    function StartGame() {
+    Name
+}
+}
